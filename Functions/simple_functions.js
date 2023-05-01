cb6ac5c19a02c20b5ca611445d4e6387ee8d44d9
Dillon Coe <SupersonicC123@gmail.com>
2023-05-01 10:46:56 -0400

    finished simple functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+function favoriteThings() {
+  console.log("My name is Dillon.");
+  console.log("My favorite movie is The Incredibles.");
+  console.log("My favorite musician Stevie Wonder.");
+}
+
+function whyImHere() {
+  console.log(
+    "I like working on computers and making things from scratch, so this tackles both of those. I also want to have financial freedom and be able to freely travel."
+  );
+}
+
+function favoritePlace() {
+  console.log(
+    "My favorite place to visit is Florida, because of the weather and visiting my family."
+  );
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
